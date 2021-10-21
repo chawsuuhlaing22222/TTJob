9b320ff9a32ea99ce8b2e00566f6066dfd8edefb
chawsuuuhlaing22222 <minnaungyoo2015@gmail.com>
2021-10-21 11:21:26 +0630

    initial commit from csh2branch

---
+++ app/src/main/java/com/me/job/tt/ui/activities/Csh2BranchActivity.kt
@@ -0,0 +1,12 @@
+package com.me.job.tt.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.me.job.R
+
+class Csh2BranchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_csh2_branch)
+    }
+}
